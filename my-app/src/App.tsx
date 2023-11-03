ad3e94684b952347e3fba0fa353a836b4337769c
Thomas Nguyen <tomxnguy@gmail.com>
2023-11-03 18:04:53 +0000

    first cj-react save

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import { Create } from './Create'
+import { useState } from 'react';
+import './App.css';
+import { Create } from './Create';
+import { Header } from './Header';
 
 const newNoteObject = {
   title: '',
   url: '',
   note: '',
-}
-
+};
 
 function App() {
-  const [nextId, setNextID] = useState(0)
-
-
+  const [nextId, setNextID] = useState(0);
 
   return (
-    <Create data={newNoteObject} nextId={nextId}/>
-  )
+    <div>
+      <Header />
+      <Create data={newNoteObject} nextId={nextId} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ my-app/src/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="header">
+      <h1>Code Journal</h1>
+      <h2>Entries</h2>
+    </div>
+  );
+}
